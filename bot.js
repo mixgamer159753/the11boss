a8d63ea43db8cb05afc69935f7bf08c118deb2f5
mixgamer159753 <44775482+mixgamer159753@users.noreply.github.com>
2018-12-02 12:30:00 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1179,6 +1179,35 @@ client.on('message', message => {
    message.channel.sendEmbed(EsTeKnAN);
   }
 });
+const Discord = require("discord.js");
+const client = new Discord.Client();
+client.on('ready', () => {
+  client.user.setGame(`on 0 servers | *help | By !SCIHuler!MIXGAMER!HulerISC#4819  .`,'https://www.twitch.tv/v5bz');
+  console.log('---------------');
+  console.log(' Bot Is Online')
+  console.log('---------------')
+});
+client.on('message', message => {
+   let embed = new Discord.RichEmbed()
 
+    let args = message.content.split(' ').slice(1).join(' ');
+     if(!message.channel.guild) return;
+if(message.content.split(' ')[0] == '#bc') {
+         message.react("✔️")
+          let embed = new Discord.RichEmbed()
+    .setColor("#FF00FF")
+    .setThumbnail(message.author.avatarURL)   
+                                      .addField('تم الارسال بواسطة :', "<@" + message.author.id + ">")
+                 message.channel.sendEmbed(embed);
+        message.guild.members.forEach(m => {
+            var bc = new Discord.RichEmbed()
+.addField('**● Sender  :**', `*** → ${message.author.username}#${message.author.discriminator}***`)
+            .addField('***● Server  :***', `*** → ${message.guild.name}***`)               
+    .setColor('#ff0000')
+                 .addField('ّ', args)
+            m.send(``,{embed: bc});
+        });
+    }
+})
 
 client.login('NTExOTc0NzA5ODYxMjg1ODkz.DsywtA.ctF5XRoAxLXnwZ9p1pBpCooBjX4');
